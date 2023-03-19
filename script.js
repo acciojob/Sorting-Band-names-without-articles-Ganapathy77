830eed384ed5ee4aea34d1e729b3155f650e0206
Ganapathy77 <106886401+Ganapathy77@users.noreply.github.com>
2023-03-19 17:57:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,28 @@
 //your code here
 
+let bandNames = ['The Allman Brothers Band', 'Amadou and Mariam', 'The Andrews Sisters', 'Red Hot Chili Peppers', 'The Beach Boys', 'The Rolling Stones'];
+
+function removeArticle(bandName){
+	let articles = ['a','an','the'];
+
+	let words = bandName.split(" ");
+
+	if(articles.includes(words[0].toLowerCase())){
+		words.shift();
+	}
+	return words.join(" ");
+}
+
+
+bandNames.sort(function(a, b) {
+	console.log(a,b)
+  return removeArticle(a).localeCompare(removeArticle(b));
+});
+
+let ulElement = document.getElementById("bands");
+
+for (let i = 0; i < bandNames.length; i++) {
+  let liElement = document.createElement("li");
+  liElement.textContent = bandNames[i];
+  ulElement.appendChild(liElement);
+}
